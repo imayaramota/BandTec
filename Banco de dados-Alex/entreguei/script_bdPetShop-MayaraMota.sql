69ef1b9e7557560f1f2d5cbd5c2cbf88c2383a8b
mayaramota <mayara.souza@bandtec.com>
2021-09-30 15:36:53 -0300

    alterados

--- Banco de dados-Alex/entreguei/script_bdPetShop-MayaraMota.sql
+++ Banco de dados-Alex/entreguei/script_bdPetShop-MayaraMota.sql
@@ -4,6 +4,7 @@ use petshop;
 create table tb_cliente(
 id_cliente int primary key auto_increment,
 nome_cliente varchar(45),
+sobrenome_cliente varchar(45),
 telefone varchar(45),
 celular varchar(45),
 endereço varchar(45)
@@ -19,11 +20,11 @@ fk_cliente int,
 foreign key (fk_cliente) references tb_cliente(id_cliente)
 ) auto_increment =101;
 
-insert into tb_cliente(nome_cliente, telefone, celular, endereço)
-values('','','',''),
-('','','',''),
-('','','',''),
-('','','','');
+insert into tb_cliente(nome_cliente, sobrenome_cliente, telefone, celular, endereço)
+values('Carla', 'Almeida','4444-8888','94586-6291','Rua das couves'),
+('Arthur','Castro','2222-5555','95635-2331','Rua das batatas'),
+('Leticia ','Castro','2222-4444','94455-2225','Av. Carlos Roberto'),
+('Alexandre ','Ferraz','6666-222','97852-2668','Rua Alameda Souza');
 
 insert into tb_pet (tipo_pet, nome_pet, raca, data_nasc_pet, fk_cliente)
 values ('cachorro','Babi','labrador','2019-01-03', 1),
@@ -34,6 +35,30 @@ values ('cachorro','Babi','labrador','2019-01-03', 1),
 
 select * from tb_pet;
 select * from tb_cliente;
-desc tb_cliente;
+
+alter table tb_cliente modify nome_cliente varchar(50);
+
+select * from tb_pet where tipo_pet = 'cachorro';
+select nome_pet, data_nasc_pet from tb_pet; 
+select * from tb_pet order by nome_pet;
+select * from tb_cliente order by endereço desc;
+select * from tb_pet where nome_pet like 'b%';
+select * from tb_cliente where sobrenome_cliente = 'Castro';
+
+update tb_cliente set telefone = '4454-2222'where id_cliente= 1;
+select * from tb_cliente;
+
+select tb_pet.*, tb_cliente.nome_cliente from tb_pet inner join tb_cliente on tb_pet.fk_cliente =  tb_cliente.id_cliente;
+/*select tb_pet.*, tb_cliente.nome_cliente as cliente from tb_pet inner join tb_cliente on tb_pet.fk_cliente = tb_cliente.id_cliente where tb_cliente.id_cliente = 2;*/
+select tb_pet.*, tb_cliente.nome_cliente as cliente from tb_pet inner join tb_cliente on tb_pet.fk_cliente = tb_cliente.id_cliente where fk_cliente = 2;
+/*select * from tb_pet where fk_cliente = 2;*/
+
+delete from tb_pet where id_pet = 103;
+select * from tb_pet;
+
+
+drop table tb_pet;
+drop table tb_cliente;
+
 
 
